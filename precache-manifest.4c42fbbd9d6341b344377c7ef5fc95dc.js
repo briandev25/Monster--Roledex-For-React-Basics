9e3cb1d7305f49b4f1cc95f524382ed2c09ffa4f
briandev25 <briankips34@gmail.com>
2020-07-01 17:19:54 +0300

    Updates

---
+++ precache-manifest.4c42fbbd9d6341b344377c7ef5fc95dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e0414ca7464a57dbf0fe30ae6ae1aac",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "c947c4f1a801df81f84a",
+    "url": "/monsters-roledex/static/css/main.10686668.chunk.css"
+  },
+  {
+    "revision": "dd45d3ee5d9c24566c0c",
+    "url": "/monsters-roledex/static/js/2.e18b89ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roledex/static/js/2.e18b89ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c947c4f1a801df81f84a",
+    "url": "/monsters-roledex/static/js/main.161f2304.chunk.js"
+  },
+  {
+    "revision": "3f342fc75b44efafabae",
+    "url": "/monsters-roledex/static/js/runtime-main.e8f54c2d.js"
+  }
+]);
